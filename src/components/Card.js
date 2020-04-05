a31a1bebd3c5e1c50dd1ca46dc29ddad8d27c376
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-04-05 18:17:31 -0400

    Project Structuring

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,10 @@
 import React from "react";
+import './Card.css';
 
 const Card = ({name, email, id}) => {
     return (
-        <div className='tc bg-light-pink dib br3-ns pa3 ma2 grow bw2 shadow-5'>
+        /*<div className='tc bg-light-yellow dib br3-ns pa3 ma2 grow bw2 shadow-5'>*/
+        <div id='backgr' className='tc dib br3-ns pa3 ma2 grow bw2 shadow-5'>
             <img alt='robots' src = {`https://robohash.org/${id}?200x200?set=set4`}/>
             <h2> {name} </h2>
             <p>{email}</p>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({searchChange}) => {
     return (
         <div className= 'pa3'>
             <input
-                className='pa3 ba b--near-white b--dotted bw3 bg-light-pink'
+                className='pa3 ba b--near-white b--dotted bw3 bg-transparent'
                 type='search'
                 placeholder='Find your robot!'
                 onChange = {searchChange}/>
