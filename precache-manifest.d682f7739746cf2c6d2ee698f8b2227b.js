8a94087cd04db25dfedeb166edc5f97f9ce90e96
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-04-05 21:33:47 -0400

    Updates

---
+++ precache-manifest.d682f7739746cf2c6d2ee698f8b2227b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485daf915af7a8b946f5b9329c092cbe",
+    "url": "/find-your-robofriends/index.html"
+  },
+  {
+    "revision": "68c92967bb42c272b095",
+    "url": "/find-your-robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "a282af3f4d545d362ffc",
+    "url": "/find-your-robofriends/static/css/main.e740f2ef.chunk.css"
+  },
+  {
+    "revision": "68c92967bb42c272b095",
+    "url": "/find-your-robofriends/static/js/2.abcc2a98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/find-your-robofriends/static/js/2.abcc2a98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a282af3f4d545d362ffc",
+    "url": "/find-your-robofriends/static/js/main.ee6f3a94.chunk.js"
+  },
+  {
+    "revision": "2e4c15e3766d4f72e910",
+    "url": "/find-your-robofriends/static/js/runtime-main.7d053f68.js"
+  },
+  {
+    "revision": "a672883ef075ce8286cd1e059a27d639",
+    "url": "/find-your-robofriends/static/media/rocketpop.a672883e.woff"
+  }
+]);
